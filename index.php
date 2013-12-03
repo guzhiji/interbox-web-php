6515aade8feb4bbbe337f2f83e220aed78dc143a
Zhiji Gu <gu_zhiji@163.com>
2013-12-03 12:45:01 +1100

    test branch created; re-structure directories for easier management

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'modules/core/core1.lib.php';
+require 'core/core1.lib.php';
 require 'core.conf.php';
 LoadIBC1Lib('common', 'framework');
 require GetSysResPath('TestPage.class.php', 'modules/pages');
